28e7638b565215e249ebc151137ad578e93bb294
gkarina <gkarina@student.21-school.ru>
2021-06-06 00:31:25 +0300

    new server

---
+++ server/server.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+// C libraries
+#include <arpa/inet.h>
+// htonl
+// htons
+// ntohl
+// ntohs
+// inet_addr
+
+#include <sys/select.h>
+// select
+
+#include <pool.h>
+// pool
+
+#include <sys/epoll.h>
+// epool
+
+#include <sys/types.h>
+// kqueue
+
+#include <sys/socket.h>
+// socket
+// accept
+// listen
+// send
+// recv
+// bind
+// connect
+// setsockopt
+// getsockname
+
+#include <fcntl.h>
+// fcntl
+
+int			main()
+{
+	return (0);
+}
