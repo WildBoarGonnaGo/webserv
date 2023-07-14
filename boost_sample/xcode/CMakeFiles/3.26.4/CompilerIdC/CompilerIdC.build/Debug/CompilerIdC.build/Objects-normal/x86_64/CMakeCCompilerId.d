81f820aed4c9a1f2abe99bd527d6eea3c133015e
WildBoarGonnaGo <unknonwthrill814@gmail.com>
2023-07-14 09:44:56 +0300

    rid off xcode directory

--- boost_sample/xcode/CMakeFiles/3.26.4/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/mzavelskiy/webserv/boost_sample/xcode/CMakeFiles/3.26.4/CompilerIdC/CMakeCCompilerId.c
--- boost_sample/xcode/build/unix_sv.build/Debug/Objects-normal/x86_64/unix_sv.d
+++
@@ -1,1 +0,0 @@
-dependencies: /Users/mzavelskiy/webserv/boost_sample/unix_sv.cpp
